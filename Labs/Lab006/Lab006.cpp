0b617d39176e937a7876b70f68931d69892d86e4
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2019-09-16 10:08:40 -0800

    Basic lab complete

---
+++ Labs/Lab006/Lab006.cpp
@@ -0,0 +1,31 @@
+/*
+Luke Underwood
+9/16/19
+Lab006.cpp
+
+*/
+
+#include<iostream>
+#include<string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+//collects a string from the user and prints it
+void stringInput()
+{
+	string input;
+	cout << "Please input a word\n";
+	cin >> input;
+
+	cout << "Your word: " << input << endl;
+	cout << "Your word is " << input.size() << " letters long.";
+}
+
+int main()
+{
+	stringInput();
+	return 0;
+}
