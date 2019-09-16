b56a1ca9cc6b19ea5651f351ce3dcaa52a0c2d2b
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2019-09-16 09:56:11 -0800

    string function

--- Labs/Lab006/Source.cpp
+++ Labs/Lab006/Source.cpp
@@ -11,12 +11,20 @@ Lab006.cpp
 
 void testVector()
 {
+	std::vector<int> v;
+	v.push_back(5);
+	v.push_back(2);
+	v.push_back(3);
 
+	for (const int& x : v)
+	{
+		std::cout << "x: " << x << std::endl;
+	}
 }
 
 int main()
 {
 
-
+	testVector();
 	return 0;
 }
