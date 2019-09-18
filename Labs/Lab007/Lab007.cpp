075cf3b58a506eec4b09fa552db7473a3b92e252
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2019-09-18 09:59:31 -0800

    Meets requirements

--- Labs/Lab007/Lab007.cpp
+++ Labs/Lab007/Lab007.cpp
@@ -2,16 +2,29 @@
 Luke Underwood
 9/18/19
 Lab007.cpp
-
+Uses functions in other files to collect data and print them
 */
 
 #include<iostream>
+#include<vector>
+#include"intio.hpp"
 
 int main()
 {
+	int i = 0;
+	std::vector<int> num;
 
+	for (i = 0; i < 3; i++)
+	{
+		std::cout << "Please input an integer.\n";
+		num.push_back(getInt());
+	}
 
-
-
+	for (i = 0; i < 3; i++) 
+	{
+		putInt(num[i], 10);
+		std::cout << std::endl;
+	}
+	
 	return 0;
 }
