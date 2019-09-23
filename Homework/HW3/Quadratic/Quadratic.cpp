32fc6d82a0c468cff3c1c05835f00630b4480957
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2019-09-23 10:51:52 -0800

    Quadratic functional

--- Homework/HW3/Quadratic/Quadratic.cpp
+++ Homework/HW3/Quadratic/Quadratic.cpp
@@ -6,10 +6,37 @@ Takes a,b,c and outputs roots of the quadratic equation
 */
 
 #include<iostream>
+#include<string>
+#include<cmath>
+
+float collectInput(std::string message)
+{
+	float i;
+	std::cout << message;
+	std::cin >> i;
+	return i;
+}
 
 int main()
 {
+	float rootminus = 0;
+	float rootplus = 0;
+	float a = collectInput("Please enter the coefficient of x^2: ");
+	float b = collectInput("Please enter the coefficient of x: ");
+	float c = collectInput("Please enter the constant: ");
+	float radical = pow(b, 2) - 4 * a * c;
 
+	if (radical >= 0)
+	{
+		rootminus = (-b - sqrt(radical)) / (2 * a);
+		rootplus = (-b + sqrt(radical)) / (2 * a);
+
+		std::cout << "Your roots are: " << rootminus << ", " << rootplus;
+	}
+	else
+	{
+		std::cout << "There are no real roots.";
+	}
 
 	return 0;
 }
