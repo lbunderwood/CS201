612a7744ef6966addd4d3c643f82e61fcf2c2f11
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2019-09-23 10:54:50 -0800

    Added comments, spacing

--- Homework/HW3/Quadratic/Quadratic.cpp
+++ Homework/HW3/Quadratic/Quadratic.cpp
@@ -9,6 +9,7 @@ Takes a,b,c and outputs roots of the quadratic equation
 #include<string>
 #include<cmath>
 
+//Prints a prompt, then returns the input
 float collectInput(std::string message)
 {
 	float i;
@@ -19,13 +20,19 @@ float collectInput(std::string message)
 
 int main()
 {
+	//initialize some variables
 	float rootminus = 0;
 	float rootplus = 0;
+
+	//collect input
 	float a = collectInput("Please enter the coefficient of x^2: ");
 	float b = collectInput("Please enter the coefficient of x: ");
 	float c = collectInput("Please enter the constant: ");
+
+	//having the contents of the square root predefined is just useful
 	float radical = pow(b, 2) - 4 * a * c;
 
+	//checks if solutions are real
 	if (radical >= 0)
 	{
 		rootminus = (-b - sqrt(radical)) / (2 * a);
