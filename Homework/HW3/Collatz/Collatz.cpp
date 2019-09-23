275fde67858468ce6b38656d9958c2b07f0361ee
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2019-09-23 09:48:55 -0800

    Collatz functional

--- Homework/HW3/Collatz/Collatz.cpp
+++ Homework/HW3/Collatz/Collatz.cpp
@@ -11,16 +11,30 @@ int main()
 {
 	int number = 0;
 
-	std::cout << "Please input an integer: ";
-	std::cin >> number;
-
+	while (number <= 0)
+	{
+		std::cout << "Please input an integer: ";
+		std::cin >> number;
+		if (number <= 0)
+		{
+			std::cout << "Please choose something larger than 0!" << std::endl;
+		} else {
+			std::cout << std::endl << number << " ";
+		}
+		
+	}
+	
 	while (number != 1)
 	{
 		switch (number % 2)
 		{
 		case 0:
+			number /= 2;
+			std::cout << number << " ";
 			break;
 		case 1:
+			number = number * 3 + 1;
+			std::cout << number << " ";
 			break;
 		}
 	}
