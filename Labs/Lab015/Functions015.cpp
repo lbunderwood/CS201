b91762f6c16a7d30920c8367ec6d1c257cee0b7c
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2019-10-07 09:50:21 -0800

    Lab015 Framework

---
+++ Labs/Lab015/Functions015.cpp
@@ -0,0 +1,9 @@
+/*
+Luke Underwood
+10/7/19
+Functions015.cpp
+Contains functions for lab 15
+*/
+
+#include"Header015.h"
+
---
+++ Labs/Lab015/Lab015.cpp
@@ -0,0 +1,13 @@
+/*
+Luke Underwood
+10/7/19
+Lab015.cpp
+contains main function for lab 15
+*/
+
+#include"Header015.h"
+
+int main()
+{
+
+}
