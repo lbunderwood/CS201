046e9a42db22e23f906c782efd9f6f077c457967
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2019-10-15 13:10:32 -0800

    functions written

--- Homework/HW4/Fifo-Lifo/filifunctions.cpp
+++ Homework/HW4/Fifo-Lifo/filifunctions.cpp
@@ -8,33 +8,40 @@
 
 void Push(std::vector<std::string>& container, const std::string& item)
 {
-
+	container.push_back(item);
+	std::cout << item << " has been added." << std::endl;
 }
 
 
 
-void fifoPop(std::vector<std::string>& container, const std::string& item)
+void fifoPop(std::vector<std::string>& container)
 {
-
+	std::cout << container[0] << " has been removed." << std::endl;
+	container.erase(container.begin());
 }
 
 
-void lifoPop(std::vector<std::string>& container, const std::string& item)
+void lifoPop(std::vector<std::string>& container)
 {
-
+	std::cout << container[0] << " has been removed." << std::endl;
+	container.pop_back();
 }
 
 
 
 bool isContainerEmpty(const std::vector<std::string>& container)
 {
-
+	if (container.size())
+		return true;
+	else
+		return false;
 }
 
 
 void printContainer(const std::vector<std::string>& container)
 {
-
+	for (auto n : container)
+		std::cout << n << std::endl;
 }
 
 
