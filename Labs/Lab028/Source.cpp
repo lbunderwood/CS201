f2959c5ebcf9d08b2106b0d526fab4f5fb37dfa7
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2019-11-20 10:22:25 -0900

    Labs

--- Labs/Lab028/Source.cpp
+++ Labs/Lab028/Source.cpp
@@ -7,28 +7,76 @@
 #include<iostream>
 #include<fstream>
 #include<iomanip>
+#include<random>
+#include<vector>
+#include<algorithm>
+#include<chrono>
+
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<int> distrib(0, 999);
+
+void printIntegersToFile(std::vector<int>& integers, std::ofstream& output)
+{
+	shuffle(integers.begin(), integers.end(), gen);
+
+	int num = 0;
+	for (int j = 0; j < 10; j++)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			output << std::setw(4) << integers[num];
+			num++;
+		}
+		output << std::endl;
+	}
+}
+
 
 int main()
 {
+	std::vector<int> integers;
+
 	std::ofstream fout("Hello.txt");
+
 	if (!fout)
 	{
 		std::cout << "ERROR OPENING FILE" << std::endl;
 	}
 	else
 	{
+		fout << "PART 1: " << std::endl;
 		for(int i = 0; i < 10; i++)
 		fout << "Hello World!\n";
 
+		fout << "PART 2: " << std::endl;
 		int num = 1;
 		for (int j = 0; j < 10; j++)
 		{
 			for (int i = 0; i < 10; i++)
 			{
 				fout << std::setw(4) << num;
+				integers.push_back(num);
 				num++;
 			}
 			fout << std::endl;
 		}
+
+		fout << std::endl;
+
+		fout << "A PART THAT I MADE UP: " << std::endl;
+		for (int j = 0; j < 10; j++)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				fout << std::setw(4) << distrib(gen);
+			}
+			fout << std::endl;
+		}
+
+		fout << std::endl;
+
+		fout << "ADDITIONAL 1: " << std::endl;
+		printIntegersToFile(integers, fout);
 	}
 }
